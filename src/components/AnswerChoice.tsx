46c327872a90465ce943e3035d3a836296addeae
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-07-30 11:51:41 +0700

    feat: add progress bar

--- src/components/AnswerChoice.tsx
+++ src/components/AnswerChoice.tsx
@@ -4,18 +4,24 @@ import { useState } from 'react'
 // TO DO is submitted and is correct
 
 interface AnswerChoiceProps {
-    isCorrect: boolean | null
+    isCorrect: boolean | null;
+    onClick? : () => void;
 }
 
-export default function AnswerChoice( {isCorrect} : AnswerChoiceProps) {
+export default function AnswerChoice( {isCorrect, onClick} : AnswerChoiceProps) {
     const [chosenAnswer, setChosenAnswer] = useState('');
+
+    const handleClick = (value: string) => {
+        setChosenAnswer(value);
+        if (onClick) onClick(); // calling parent handler
+    }
 
     return (
         <div className='flex justify-between gap-3'>
-            <button 
+            <button
                 className={`flex items-center gap-2 w-full rounded-lg border-1 p-2 cursor-pointer hover:bg-[#FAE8ED] transition
                     ${chosenAnswer === 'True' ? 'bg-[#FAE8ED] border-2 border-[#A1133B]' : 'border-[#D9D9D9] bg-white'}`}
-                onClick={() => setChosenAnswer('True')}
+                onClick={() => handleClick('True')}
             >
                 {chosenAnswer === 'True' ? <CircleDot className='text-[#A1133B]'/> :  <Circle className='text-[#D9D9D9]'/> }
                 <span
@@ -27,7 +33,7 @@ export default function AnswerChoice( {isCorrect} : AnswerChoiceProps) {
             <button
                 className={`flex items-center gap-2 w-full rounded-lg border-1 p-2 cursor-pointer hover:bg-[#FAE8ED] transition
                     ${chosenAnswer === 'False' ? 'bg-[#FAE8ED] border-2 border-[#A1133B]' : 'border-[#D9D9D9] bg-white'}`}  
-                    onClick={() => setChosenAnswer('False')}  
+                    onClick={() => handleClick('False')}  
             >
                 {chosenAnswer === 'False' ? <CircleDot className='text-[#A1133B]'/> :  <Circle className='text-[#D9D9D9]'/> }
                 <span
--- src/components/AssessmentCard.tsx
+++ src/components/AssessmentCard.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AnswerChoice from "./AnswerChoice"
+import { Progress } from "./ui/progress"
 
 export interface AssessmentCardProps {
     title: string,
@@ -21,14 +22,23 @@ export const AssessmentCard = ( {
     score
 } : AssessmentCardProps) => {
     const [userAnswers, setUserAnswers] = useState<(string | null)[]>([]);
+    const [progress, setProgress] = useState(0);
+    const [answeredIndex, setAnsweredIndex] = useState<number[]>([])
+
+    const handleProgress = (index: number) => {
+        if (!answeredIndex.includes(index)) {
+            setAnsweredIndex((prev) => [...prev, index]);
+            setProgress(((answeredIndex.length + 1) / questions.length) * 100);
+        }
+    }
 
     return (
         <section className="flex flex-col rounded-lg border-1 border-[#D9D9D9] items-center">
             {/* Header */}
-            <div className="flex justify-between items-center bg-[#FAE8ED] p-4 rounded-t-lg w-full">
+            <div className="flex justify-between items-center bg-[#FAE8ED] py-4 px-7 rounded-t-lg w-full">
                 {/* Title and Module Name */}
                 <div className="flex flex-col">
-                    <span className="font-semibold">{title}</span>
+                    <span className="font-semibold text-lg">{title}</span>
                     <span className="text-[#383838] text-[10px]">{module}</span>
                 </div>
                 {isSubmitted && (
@@ -37,21 +47,25 @@ export const AssessmentCard = ( {
                     </div>
                 )}
             </div>
-            {/* Questions Section */}
+            {/* Progress & Questions Section */}
             <div className="flex flex-col gap-3 my-5 mx-4">
+                <Progress value={progress} className="w-full mb-3"/>
                 {questions.map((question, index) => (
                     <div key={index} className="flex flex-col border-1 border-[#D9D9D9] rounded-lg p-3 gap-2">
-                        <div>
+                        <div className="text-sm">
                             <span>{index+1}.</span>
                             <span> {question} </span>
                         </div>
                         {/* Answers Choice */}
-                        <AnswerChoice isCorrect={null}/>
+                        <AnswerChoice 
+                            isCorrect={null}
+                            onClick={() => handleProgress(index)}    
+                        />
                     </div>
                 ))}
             </div>
             {/* Submit Assessment */}
-            <button className="w-5/6 bg-[#C9184A] text-white py-1 rounded-lg mb-5 cursor-pointer transform transition hover:-translate-y-1">
+            <button className="w-5/6 text-sm bg-[#C9184A] text-white py-2 rounded-lg mb-5 cursor-pointer transform transition hover:-translate-y-1">
                 Kumpulkan
             </button>
         </section>

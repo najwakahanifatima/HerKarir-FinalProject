fb96a26041c472732e7edc3535b346c0b3948a8c
Najwa Kahani Fatima <najwanewa1010@gmail.com>
2025-07-30 17:02:20 +0700

    feat: clickable and routing for start button to details course

--- src/components/CourseCard.tsx
+++ src/components/CourseCard.tsx
@@ -1,4 +1,7 @@
+"use client"
+
 import Image from "next/image"
+import { useRouter } from "next/navigation";
 
 export interface CourseCardProps {
   title: string;
@@ -11,6 +14,11 @@ export const CourseCard = ({
   institution,
   imagePath
 }: CourseCardProps) => {
+  const router = useRouter();
+  const handleStart = () => {
+    router.push(`/course/${encodeURIComponent(title)}`)
+  }
+  
   return (
     <div className="rounded-xl flex flex-col md:flex-row gap-3 w-full h-full shadow-md items-center p-4 bg-white">
       
@@ -18,7 +26,9 @@ export const CourseCard = ({
       <div className="flex flex-col gap-2 items-start w-full sm:w-2/3 text-center sm:text-left">
         <span className="text-sm font-semibold w-full">{title}</span>
         <span className="text-[10px] text-gray-500 w-full">{institution}</span>
-        <button className="mt-1 border border-[#C9184A] text-[#C9184A] cursor-pointer 
+        <button 
+          onClick={handleStart}
+          className="mt-1 border border-[#C9184A] text-[#C9184A] cursor-pointer 
           px-4 py-1 text-[12px] rounded-full hover:bg-[#C9184A] hover:text-white transition">
           Mulai
         </button>
